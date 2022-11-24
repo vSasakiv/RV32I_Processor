de93eacfc576a79ffd51e0aadec757f053250d74
vSasakiv <vitorsakivenzelv@gmail.com>
2022-11-24 16:02:10 -0300

    :pencil: adding comments to reg32bit

--- src/cpu/DataFlow/reg32bit_mod.v
+++ src/cpu/DataFlow/reg32bit_mod.v
@@ -1,10 +1,13 @@
+/* Módulo que descreve um registrador de 32 bits */
 module reg32bit (
-    input clk,
-    input rs_i,
-    input [31:0] data_i,
-    output reg [31:0] data_o
+    input clk, // clock de entrada
+    input rs_i, // entrada de reset síncrono
+    input [31:0] data_i, // valor de entrada
+    output reg [31:0] data_o // valor de saída
 );
-    
+    /* A cada borda de clock devemos verificar se o reset está ativo, caso esteja
+    resetamos o valor do registrador, caso não esteja, apenas passamos o valor da entrada
+    para dentro do registrador */
     always @(posedge clk) begin
       if (rs_i) begin
         data_o <= 32'h00000000;
--- src/cpu/DataFlow/reg32bit_test.v
+++ src/cpu/DataFlow/reg32bit_test.v
@@ -1,15 +1,21 @@
+/* Testbench para o registrador de 32 bits. Sorteamos valores aleatórios para a entrada
+do registrador, e verificamos se o valor realmente foi armazenado e por fim ativamos o reset
+e verificamos se o valor dele realmente está igual a 0 */
 `timescale 1 ns / 100 ps
 
 module reg32bit_TB ();
-    reg clk;
-    wire [31:0] data_o;
-    reg [31:0] data_i;
+    reg clk; // entrada sinal de clock
+    reg reset; // sinal de reset
+    wire [31:0] data_o; // saída do registrador
+    reg [31:0] data_i; // entrada do registrador
     integer i;
 
-    reg32bit UUT (.clk(clk), .data_i(data_i), .data_o(data_o));
+    // módulo em teste: registrador de 32 bits
+    reg32bit UUT (.clk(clk), .data_i(data_i), .data_o(data_o), .rs_i(reset));
 
     initial begin
         clk = 0;
+        reset = 0;
         for (i = 0; i < 50; i = i + 1) begin
             data_i = $urandom;
             #10;
@@ -20,9 +26,14 @@ module reg32bit_TB ();
             end
             clk = ~clk;
         end
+        reset = 1;
+        #10
+        clk = ~clk;
+        #10
+        if (data_o !== 32'h00000000) begin
+          $display("Error: data_o = %d data_i = %d", data_o, data_i);
+        end
         $display("Finished");
         $stop;
     end
-    
-
 endmodule
