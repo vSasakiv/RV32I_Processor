2402e83e31bb7dd35ce8e1ae47480693b05a6ec1
vSasakiv <vitorsakivenzelv@gmail.com>
2022-11-24 15:54:36 -0300

    :pencil: adicionando comentários mux4to1

--- src/cpu/DataFlow/mux4to1_mod.v
+++ src/cpu/DataFlow/mux4to1_mod.v
@@ -1,17 +1,18 @@
+/* Módulo que representa um multiplexador geral de 4 entradas de 32-bits e uma saída de 32-bits */
 module mux4to1 (
-  input [1:0] select,
-  input [31:0] I0, I1, I2, I3,
-  output [31:0] data_o
+  input [1:0] select, // seletor do multiplexador
+  input [31:0] I0, I1, I2, I3, // entradas
+  output [31:0] data_o // saída
 );
 
-  reg [31:0] data_o_r;
+  reg [31:0] data_o_r; // reg para ser utilizado no bloco always
 
   always @(*) begin
     case (select)
-      2'd0: data_o_r = I0;
-      2'd1: data_o_r = I1;
-      2'd2: data_o_r = I2;
-      2'd3: data_o_r = I3;
+      2'd0: data_o_r = I0; // caso o seletor for 00, I0 deve passar para a saída
+      2'd1: data_o_r = I1; // caso o seletor for 01, I1 deve passar para a saída
+      2'd2: data_o_r = I2; // caso o seletor for 10, I2 deve passar para a saída
+      2'd3: data_o_r = I3; // caso o seletor for 11, I3 deve passar para a saída
     endcase
   end
   assign data_o = data_o_r;
--- src/cpu/DataFlow/mux4to1_test.v
+++ src/cpu/DataFlow/mux4to1_test.v
@@ -1,11 +1,15 @@
+/* Testbench para o módulo do multiplexador 2 para 1, neste teste novamente são sorteados
+entrdas aleatórias, e para cada par de entradas, é testado os dois estados possíveis do select,
+e isso é exposto ao terminal por meio do $monitor */
 `timescale 1 ns / 100 ps
 
 module mux4to1_TB ();
-    wire [31:0] data_o;
-    reg [1:0] select;
-    reg [31:0] I0, I1, I2, I3;
+    wire [31:0] data_o; // saída do multiplexador
+    reg [1:0] select; // seletor
+    reg [31:0] I0, I1, I2, I3; // entradas do multiplexador
     integer i, j;
 
+    // módulo em teste: multiplexaxdor 4 entradas
     mux4to1 UUT (.select(select), .I0(I0), .I1(I1), .I2(I2), .I3(I3), .data_o(data_o));
 
     initial begin
