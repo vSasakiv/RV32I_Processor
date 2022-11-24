4483082af8fd169b6770810e13c13b96a31081c3
vSasakiv <vitorsakivenzelv@gmail.com>
2022-11-24 15:52:34 -0300

    :pencil: adicionando comentários no mux2to1

--- src/cpu/DataFlow/mux2to1_test.v
+++ src/cpu/DataFlow/mux2to1_test.v
@@ -1,11 +1,15 @@
+/* Testbench para o módulo do multiplexador 2 para 1, neste teste novamente são sorteados
+entrdas aleatórias, e para cada par de entradas, é testado os dois estados possíveis do select,
+e isso é exposto ao terminal por meio do $monitor */
 `timescale 1 ns / 100 ps
 
 module mux2to1_TB ();
-    wire [31:0] data_o;
-    reg select;
-    reg [31:0] I0, I1;
+    wire [31:0] data_o; // saída do multiplexador
+    reg select; // seletor
+    reg [31:0] I0, I1; // entradas do multiplexador
     integer i, j;
 
+    // módulo em teste: multiplexador de 2 entradas
     mux2to1 UUT (.select(select), .I0(I0), .I1(I1), .data_o(data_o));
 
     initial begin
